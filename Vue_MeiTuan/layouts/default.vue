fcf1597470761b27dcbccadc20df9098fdb8966a
Sherlock Formalin <Fri_Liu@163.com>
2018-12-11 17:56:00 +0800

    18.12.11

---
+++ Vue_MeiTuan/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <el-container>
+    <el-header>
+      <my-header />
+    </el-header>
+    <el-main>
+      <nuxt />
+    </el-main>
+    <el-footer height="100%">
+      <my-footer />
+    </el-footer>
+  </el-container>
+</template>
+
+<script>
+  import MyFooter from '@/components/public/footer/footer.vue'
+  import MyHeader from '@/components/public/header/header.vue'
+  export default {
+    components:{
+      MyHeader,
+      MyFooter
+    }
+  }
+</script>
+
